aedbc5e4867fb2403de22bf3a909f6090390c203
Sport97 <por21022@byui.edu>
2025-03-14 17:46:00 -0500

    my set-up and added an index get route, only for clarification purposes

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,33 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const swaggerDocs = require('./swagger');
-const authRoutes = require('./auth');
-const usersRoutes = require('./users');
-const candyRoutes = require('./candy');
-const storesRoutes = require('./stores');
-const ordersRoutes = require('./orders');
-const utilities = require('../utilities/index');
+const swaggerDocs = require("./swagger");
+const authRoutes = require("./auth");
+const usersRoutes = require("./users");
+const candyRoutes = require("./candy");
+const storesRoutes = require("./stores");
+const ordersRoutes = require("./orders");
+const utilities = require("../utilities/index");
+
+router.get("/", require("../controllers/index").buildIndex);
 
 // Mount the Swagger routes to serve the API documentation
-router.use('/', utilities.handleErrors(swaggerDocs));
+router.use("/", utilities.handleErrors(swaggerDocs));
 
 // Mount the sub-router to handle all routes under /authRoutes
-router.use('/auth', utilities.handleErrors(authRoutes));
+router.use("/auth", utilities.handleErrors(authRoutes));
 
 // Mount a sub-router to handle all routes under /user
-router.use('/users', utilities.handleErrors(usersRoutes));
+router.use("/users", utilities.handleErrors(usersRoutes));
 
 // Mount a sub-router to handle all routes under /candy
-router.use('/candy', utilities.handleErrors(candyRoutes));
+router.use("/candy", utilities.handleErrors(candyRoutes));
 
 // Mount a sub-router to handle all routes under /stores
-router.use('/stores', utilities.handleErrors(storesRoutes));
+router.use("/stores", utilities.handleErrors(storesRoutes));
 
 // Mount a sub-router to handle all routes under /orders
-router.use('/orders', utilities.handleErrors(ordersRoutes));
+router.use("/orders", utilities.handleErrors(ordersRoutes));
 
 module.exports = router;
